f42bbdc607900812e403df07630d5cb9ed6b71ef
pdoms <domspaulo@gmail.com>
2021-10-28 14:10:52 +0200

    linear regression height_weight

--- src/examples/dummydata.py
+++ src/examples/dummydata.py
@@ -1,25 +1,23 @@
 
 import sys
 sys.path.append("..")
-from tools.linear_regression.linear_regression import least_squares
-from tools.linear_regression.analysis import r_square, adj_r_square, tss, rss, ess, standard_error_of_regression
-import math
+from tools.linear_regression.linear_regression import LinReg
+
 weight = [241, 162, 212, 220, 206, 152,183,167,175,156,186,213,167,189,186,172, 196]
 height = [73, 68, 74, 71,69,67,68,68,67,63,71,71,64,69,69,67,72]
 
 
-alpha, beta = least_squares(weight, height)
-print('COEFFICIENTS', alpha, beta)
-""" print(tss(height))
-print(rss(weight, height, alpha, beta))
-print(ess(weight, height, alpha, beta)) """
-print('R2', r_square(weight, height, alpha, beta))
-print('multipleR', math.sqrt(r_square(weight, height, alpha, beta)))
-print('adjR2', adj_r_square(weight, height, alpha, beta))
-print('SE', standard_error_of_regression(weight, height, alpha, beta))
+reg = LinReg(weight, height, ['height', 'weight'])
+reg.fit()
+results = reg.analyse(se_samples=10)
 
 
 
+#predict
+#make polynominal? 
+#add_column
+#remove_column
+#evaluate --- but only when I ran some tests
 
-#std error
+
 
---
+++ src/examples/weight_height.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+sys.path.append("..")
+from tools.linear_regression.linear_regression import LinReg
+
+#linear regression with one independent variable 
+
+#load dataset
+dir_path = os.path.dirname(os.path.realpath(__file__))
+source = os.path.join(dir_path, 'data', 'weight-height.csv')
+df = pd.read_csv(source)
+
+#restrict to one gender
+females = df[df['Gender'] == 'Female']
+
+#determine independent (x) and dependent (y) variable
+x = females['Weight'].tolist()
+y = females['Height'].tolist()
+
+#initialize regression class
+regress = LinReg(x,y, columns=['weight', 'height'])
+
+#describe dependent variable
+regress.y_stats(report=True, write='results/weight_height.txt')
+
+#fit data - use OLS
+regress.fit(method='ordinary')
+regress.analyse(se_samples=20, report=True, write='results/weight_height.txt')
+regress.evaluate()
+
+#visualize results
+plt.scatter(x,y)
+plt.title('Female Height and Weight (5000 samples) inkl. Best Fit')
+plt.ylabel('Height in inches')
+plt.xlabel('Weight in lbs')
+plt.plot(x, [regress.beta[0] + regress.beta[1]*xi for xi in x], color='red')
+plt.savefig('results/weight_height_graph.png') 
+plt.show()
+
+#predict 
+prediction = regress.predict(120)
+print('Prediction: the person will weigh', prediction, 'lbs')
+
+
+
+
+
+
